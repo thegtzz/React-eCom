eef27ece56550d8923cd0701f6fbfd695b882b50
thegtzz <thegtzz@gmail.com>
2020-08-12 20:55:49 +0300

    Components created/modified

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import {Button} from "./Button";
 import {Header} from "./Header";
 import {Categories} from "./Categories";
+import {SortPopUp} from "./SortPopUp";
 
-export {Button, Header, Categories}
+export {Button, Header, Categories, SortPopUp}
