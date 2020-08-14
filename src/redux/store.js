35d80ae241018fc2283eb6021b40f3b268c3a4b7
thegtzz <thegtzz@gmail.com>
2020-08-14 20:25:08 +0300

    thunk added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,12 @@
-import {createStore} from "redux";
+import {createStore, compose, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
 import {rootReducer} from "./reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk)))
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 window.store = store
